1c543736507a74d4567400f4c858f01d33ac4ae8
Masato Seki <masatoseki@the-neighbourhood.com>
2015-09-30 23:20:30 +0100

    WIP case study carousel

--- src/scripts/modules/case-study-carousel.js
+++ src/scripts/modules/case-study-carousel.js
@@ -1,20 +1,22 @@
 var Flickity = require('flickity');
-var $homeCarousel = $('.case-study-carousel');
+var $caseStudyCarousel = $('.c');
 
 var init = function() {
-    if($homeCarousel.length) {
+/*
+    if($caseStudyCarousel.length) {
         // element argument can be a selector string
         //   for an individual element
-        var flkty = new Flickity( '.case-study-carousel', {
+        var flkty = new Flickity( '.c', {
             autoPlay: false,
             pageDots: true,
             wrapAround: true,
-            setGallerySize: true,
+            setGallerySize: false,
             cellSelector: '.case-study-carousel__cell',
             selectedAttraction: 0.01,
             friction: 0.15
         });
     }
+*/
 }
 
 exports.init = init;
