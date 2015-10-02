641d0c9cc8d49b7845e0c4cb711b1b7661bf3a74
Masato Seki <masatoseki@the-neighbourhood.com>
2015-10-02 23:30:25 +0100

    WIP case study carousel control styling

--- src/scripts/modules/case-study-carousel.js
+++ src/scripts/modules/case-study-carousel.js
@@ -1,19 +1,25 @@
 var Flickity = require('flickity');
-var $caseStudyCarousel = $('.c');
+var $caseStudyCarousel = $('.case-study-carousel');
 
 var init = function() {
 /*
     if($caseStudyCarousel.length) {
         // element argument can be a selector string
         //   for an individual element
-        var flkty = new Flickity( '.c', {
+        var flkty = new Flickity( '.case-study-carousel', {
             autoPlay: false,
             pageDots: true,
             wrapAround: true,
             setGallerySize: false,
             cellSelector: '.case-study-carousel__cell',
             selectedAttraction: 0.01,
-            friction: 0.15
+            friction: 0.15,
+            arrowShape: { 
+                x0: 10,
+                x1: 50, y1: 30,
+                x2: 50, y2: 20,
+                x3: 30
+            }
         });
     }
 */
