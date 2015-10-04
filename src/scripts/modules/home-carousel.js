e016c56dd99a0141110a44ba4d6afec63321d792
Masato Seki <masatoseki@the-neighbourhood.com>
2015-10-04 17:02:02 +0100

    WIP get image caption on home carousel

--- src/scripts/modules/home-carousel.js
+++ src/scripts/modules/home-carousel.js
@@ -3,25 +3,33 @@ var Flickity = require('flickity');
 var init = function() {
     var homeCarousel = document.querySelector('.home-carousel');
     var newsCarousel = document.querySelector('.news__item');
-    var flkty = new Flickity( homeCarousel, {
-        autoPlay: true,
-        pageDots: false,
-        wrapAround: true,
-        setGallerySize: false,
-        cellSelector: '.home-carousel__cell',
-        selectedAttraction: 0.01,
-        friction: 0.15
+    $(function() {
+        var flkty = new Flickity( homeCarousel, {
+            imagesLoaded: true,
+            autoPlay: 4000,
+            pageDots: false,
+            wrapAround: true,
+            setGallerySize: false,
+            cellSelector: '.home-carousel__cell',
+            selectedAttraction: 0.01,
+            friction: 0.15
+        });
+        $('.caption--project').text(flkty.selectedElement);
+
+        flkty.on( 'cellSelect', function() {
+            console.log(homeCarousel, flkty.selectedElement.alt);
+            $('.caption--project').text(homeCarousel, flkty.selectedElement.alt);
+        });
     });
-    
+
     var flkty = new Flickity( newsCarousel, {
-        autoPlay: 5000,
+        autoPlay: 4000,
         draggable: false,
         prevNextButtons: false,
         pageDots: false,
         wrapAround: true,
         setGallerySize: true,
-        cellSelector: '.news__cell',
-        friction: 0.4
+        cellSelector: '.news__cell'
     });
 
 }
