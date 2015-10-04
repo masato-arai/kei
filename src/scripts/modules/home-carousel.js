7b8bf906c1062cf1184ed078c5dc51f9a5250a53
Masato Seki <masatoseki@the-neighbourhood.com>
2015-10-04 20:38:30 +0100

    Adding home carousel image link

--- src/scripts/modules/home-carousel.js
+++ src/scripts/modules/home-carousel.js
@@ -1,40 +1,39 @@
 var Flickity = require('flickity');
 
-
-
 var init = function() {
-    var homeCarousel = document.querySelector('.home-carousel');
-    var newsCarousel = document.querySelector('.news__item');
-    $(function() {
-        var flkty = new Flickity( homeCarousel, {
-            imagesLoaded: true,
-            autoPlay: 6000,
-            pageDots: false,
-            wrapAround: true,
-            setGallerySize: false,
-            cellSelector: '.home-carousel__cell',
-            selectedAttraction: 0.01,
-            friction: 0.15
-        });
-
-        var projectName = $(flkty.selectedElement).find('.home-carousel__link').find('.home-carousel__img').attr("alt");
-        $('.caption--project').text(projectName);
-        flkty.on( 'cellSelect', function() {
+    if($('.home-carousel').length && $('.news__item').length) {
+        var homeCarousel = document.querySelector('.home-carousel');
+        var newsCarousel = document.querySelector('.news__item');
+        $(function() {
+            var flkty = new Flickity( homeCarousel, {
+                imagesLoaded: true,
+                autoPlay: 6000,
+                pageDots: false,
+                wrapAround: true,
+                setGallerySize: false,
+                cellSelector: '.home-carousel__cell',
+                selectedAttraction: 0.01,
+                friction: 0.15
+            });
+    
             var projectName = $(flkty.selectedElement).find('.home-carousel__link').find('.home-carousel__img').attr("alt");
             $('.caption--project').text(projectName);
+            flkty.on( 'cellSelect', function() {
+                var projectName = $(flkty.selectedElement).find('.home-carousel__link').find('.home-carousel__img').attr("alt");
+                $('.caption--project').text(projectName);
+            });
         });
-    });
-
-    var flkty = new Flickity( newsCarousel, {
-        autoPlay: 4000,
-        draggable: false,
-        prevNextButtons: false,
-        pageDots: false,
-        wrapAround: true,
-        setGallerySize: true,
-        cellSelector: '.news__cell'
-    });
-
+    
+        var flkty = new Flickity( newsCarousel, {
+            autoPlay: 4000,
+            draggable: false,
+            prevNextButtons: false,
+            pageDots: false,
+            wrapAround: true,
+            setGallerySize: true,
+            cellSelector: '.news__cell'
+        });
+    }
 }
 
 exports.init = init;
